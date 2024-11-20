f42be9607d19fb8891b4399857ff1a5a1c09e7b9
Fluka <m.benabdelkader02@gmail.com>
2024-11-20 12:28:42 +0100

    add error checks

--- Server.c
+++ Server.c
@@ -14,35 +14,38 @@ struct Server server_constructor(int domain, int service, int protocol,
     server.interface = interface;
     server.start = start;
 
-    server.address.sin_family = domain;
-    server.address.sin_port = htons(port);
-    server.address.sin_addr.s_addr = htonl(interface);
+    server.address.sin_family = domain; // AF_INET for IPv4
+    server.address.sin_port = htons(port); 
+    server.address.sin_addr.s_addr = htonl(interface); 
 
     server.socket = socket(domain, service, protocol);
 
-    if (server.sock == 0) {
-        perror("Failed to create Socket");
+    // Check if the socket was created successfully
+    if (server.sock == 0) 
+    {
+        perror("Failed to create Socket...\n");
         exit(1); 
     }
 
+    //
     if (setsockopt(server.sock, SOL_SOCKET, SO_REUSEADDR, &(int){1},
                     sizeof(int)) < 0)
     perror("setsockopt(SO_REUSEADDR) failed");
 
-    int b = bind(server.sock, (struct sockaddr *)&server.address,
-                sizeof(server.address));
-
-    if (b < 0) {
-    perror("Failed to bind Socket");
-    exit(1);
+    if (bind(server.sock, (struct sockaddr *)&server.address,
+            sizeof(server.address)) < 0)
+    {
+        perror("Failed to bind Socket...\n");
+        exit(1);
     }
 
-    int l = listen(server.sock, server.backlog);
-
-    if (l < 0) {
-    perror("Failed to listen on Socket");
-    exit(1);
+    if (listen(server.sock, server.backlog) < 0) 
+    {
+        perror("Failed to listen on Socket");
+        exit(1);
     }
+
+    server.start = start;
 
     return server;
 }
