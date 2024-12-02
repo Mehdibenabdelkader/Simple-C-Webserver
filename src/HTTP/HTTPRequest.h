b4c000844491457deb6edbfa81e6bf4c8f24b295
Fluka <m.benabdelkader02@gmail.com>
2024-12-02 12:17:19 +0100

    add body and header fields to HTTPRequest structure

--- src/HTTP/HTTPRequest.h
+++ src/HTTP/HTTPRequest.h
@@ -13,8 +13,20 @@ struct HTTPRequest{
     int method;
     char *URI;
     float version;
-
+    // can i include the header fields and message body here?
+    char *body;
+    struct HeaderField *headerFields;
 };
 
+struct HeaderField{
+    char *name;
+    char *value;
+    struct HeaderField *next;
+};
+
+
+
 struct HTTPRequest HTTPRequest_constructor(char *requestString);
+struct HeaderField HeaderField_constructor(char *name, char *value);
+void printHTTPRequest(struct HTTPRequest request);
 #endif // HTTPREQUEST_H
